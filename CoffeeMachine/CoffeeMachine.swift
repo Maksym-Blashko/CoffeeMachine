fa808df8a907f95f854e38ec38f82cca11126560
Blashko Maksym <Blashkoma@gmail.com>
2021-02-16 13:00:12 +0200

    first work version

--- CoffeeMachine/CoffeeMachine.swift
+++ CoffeeMachine/CoffeeMachine.swift
@@ -7,6 +7,118 @@
 
 import Foundation
 
+enum CoffeeOptions {
+    case espresso, cappuccino, americano, tea
+    }
+
+enum IngredientsForCoffeeMachine {
+    case water, milk, coffee, trash
+}
+
 class CoffeeMachine {
+   
+    // tanks
+    var coffeeTank: Int = 0
+    var milkTank: Int = 0
+    var waterTank: Int = 0
+    var trashTank: Int = 0
+    // max value
+    let maxValueCoffee = 500
+    let maxValueMilk = 500
+    let maxValueWater = 500
+    let maxValueTrash = 500
+    // info
+    var textInfoPanel: String = "What would you like a drink?"
+
+    func makingDrink(drink: CoffeeOptions) {
+        if checkEnoughQtyForDrink(drink: drink) {
+            updateQty(drink: drink)
+            textInfoPanel = "Enjoy"
+        }
+    }
     
+    private func checkEnoughQtyForDrink(drink: CoffeeOptions) -> Bool {
+        
+        textInfoPanel = ""
+        
+        if waterTank < ingredientsForCooking(drink: drink, ingredients: .water) {
+            if textInfoPanel.isEmpty {
+                textInfoPanel = "Fill water tank"
+            } else {
+                textInfoPanel = textInfoPanel + "\n" + "Fill water tank"
+            }
+        }
+        if milkTank < ingredientsForCooking(drink: drink, ingredients: .milk) {
+            if textInfoPanel.isEmpty {
+                textInfoPanel = "Fill milk tank"
+            } else {
+                textInfoPanel = textInfoPanel + "\n" + "Fill milk tank"
+            }
+        }
+        if coffeeTank < ingredientsForCooking(drink: drink, ingredients: .coffee) {
+            if textInfoPanel.isEmpty {
+                textInfoPanel = "Fill coffee tank"
+            } else {
+                textInfoPanel = textInfoPanel + "\n" + "Fill coffee tank"
+            }
+        }
+        if (trashTank + ingredientsForCooking(drink: drink, ingredients: .trash)) > 500 {
+            if textInfoPanel.isEmpty {
+                textInfoPanel = "Dregdrawer full"
+            } else {
+                textInfoPanel = textInfoPanel + "\n" + "Dregdrawer full"
+            }
+        }
+        
+        if textInfoPanel.isEmpty { return true } else { return false }
+    }
+   
+    private func ingredientsForCooking(drink: CoffeeOptions, ingredients: IngredientsForCoffeeMachine) -> Int {
+        
+        if drink == CoffeeOptions.espresso && ingredients == IngredientsForCoffeeMachine.water { return 30 }
+        if drink == CoffeeOptions.espresso && ingredients == IngredientsForCoffeeMachine.milk { return 0 }
+        if drink == CoffeeOptions.espresso && ingredients == IngredientsForCoffeeMachine.coffee { return 100 }
+        if drink == CoffeeOptions.espresso && ingredients == IngredientsForCoffeeMachine.trash { return 100 }
+
+        if drink == CoffeeOptions.cappuccino && ingredients == IngredientsForCoffeeMachine.water { return 100 }
+        if drink == CoffeeOptions.cappuccino && ingredients == IngredientsForCoffeeMachine.milk { return 100 }
+        if drink == CoffeeOptions.cappuccino && ingredients == IngredientsForCoffeeMachine.coffee { return 50 }
+        if drink == CoffeeOptions.cappuccino && ingredients == IngredientsForCoffeeMachine.trash { return 50 }
+
+        if drink == CoffeeOptions.americano && ingredients == IngredientsForCoffeeMachine.water { return 200 }
+        if drink == CoffeeOptions.americano && ingredients == IngredientsForCoffeeMachine.milk { return 0 }
+        if drink == CoffeeOptions.americano && ingredients == IngredientsForCoffeeMachine.coffee { return 100 }
+        if drink == CoffeeOptions.americano && ingredients == IngredientsForCoffeeMachine.trash { return 100 }
+
+        if drink == CoffeeOptions.tea && ingredients == IngredientsForCoffeeMachine.water { return 250 }
+        if drink == CoffeeOptions.tea && ingredients == IngredientsForCoffeeMachine.milk { return 0 }
+        if drink == CoffeeOptions.tea && ingredients == IngredientsForCoffeeMachine.coffee { return 0 }
+        if drink == CoffeeOptions.tea && ingredients == IngredientsForCoffeeMachine.trash { return 0 }
+        
+        return 0
+    }
+
+    private func updateQty(drink: CoffeeOptions) {
+        waterTank -= ingredientsForCooking(drink: drink, ingredients: .water)
+        milkTank -= ingredientsForCooking(drink: drink, ingredients: .milk)
+        coffeeTank -= ingredientsForCooking(drink: drink, ingredients: .coffee)
+        trashTank += ingredientsForCooking(drink: drink, ingredients: .trash)
+    }
+    
+    func addIngredient(ingredient: IngredientsForCoffeeMachine) {
+        switch ingredient {
+        case .coffee:
+            coffeeTank = maxValueCoffee
+            textInfoPanel = "Coffee tank is full"
+        case .milk:
+            milkTank = maxValueMilk
+            textInfoPanel = "Milk tank is full"
+        case .water:
+            waterTank = maxValueWater
+            textInfoPanel = "Water tank is full"
+        case .trash:
+            trashTank = 0
+            textInfoPanel = "Empty bin"
+        }
+    }
 }
--- CoffeeMachine/ViewController.swift
+++ CoffeeMachine/ViewController.swift
@@ -9,11 +9,101 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let coffeeMachine = CoffeeMachine()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        infoPanel.text = coffeeMachine.textInfoPanel
     }
-
-
+    
+    @IBOutlet weak var progressWater: UIProgressView!
+    @IBOutlet weak var progressMilk: UIProgressView!
+    @IBOutlet weak var progressCoffee: UIProgressView!
+    @IBOutlet weak var progressBin: UIProgressView!
+    @IBOutlet weak var infoPanel: UITextField!
+    
+    func upDateProgress(currentProgress: UIProgressView) {
+        if currentProgress == progressWater {
+            currentProgress.progress = (Float(coffeeMachine.waterTank) / Float(coffeeMachine.maxValueWater))
+        }
+        if currentProgress == progressCoffee {
+            currentProgress.progress = (Float(coffeeMachine.coffeeTank) / Float(coffeeMachine.maxValueCoffee))
+        }
+        if currentProgress == progressMilk {
+            currentProgress.progress = (Float(coffeeMachine.milkTank) / Float(coffeeMachine.maxValueMilk))
+        }
+        if currentProgress == progressBin {
+            currentProgress.progress = (Float(coffeeMachine.trashTank) / Float(coffeeMachine.maxValueTrash))
+        }
+        
+        infoPanel.text = coffeeMachine.textInfoPanel
+    }
+    
+    @IBAction func makeEspressoButtonAction(_ sender: UIButton) {
+        coffeeMachine.makingDrink(drink: .espresso)
+        
+        upDateProgress(currentProgress: progressWater)
+        upDateProgress(currentProgress: progressMilk)
+        upDateProgress(currentProgress: progressCoffee)
+        upDateProgress(currentProgress: progressBin)
+    }
+    @IBAction func makeCappuccinoButtonAction(_ sender: UIButton) {
+        coffeeMachine.makingDrink(drink: .cappuccino)
+        
+        upDateProgress(currentProgress: progressWater)
+        upDateProgress(currentProgress: progressMilk)
+        upDateProgress(currentProgress: progressCoffee)
+        upDateProgress(currentProgress: progressBin)
+    }
+    @IBAction func makeAmericanoButtonAction(_ sender: UIButton) {
+        coffeeMachine.makingDrink(drink: .americano)
+        
+        upDateProgress(currentProgress: progressWater)
+        upDateProgress(currentProgress: progressMilk)
+        upDateProgress(currentProgress: progressCoffee)
+        upDateProgress(currentProgress: progressBin)
+    }
+    @IBAction func makeTeaButtonAction(_ sender: UIButton) {
+        coffeeMachine.makingDrink(drink: .tea)
+        
+        upDateProgress(currentProgress: progressWater)
+        upDateProgress(currentProgress: progressMilk)
+        upDateProgress(currentProgress: progressCoffee)
+        upDateProgress(currentProgress: progressBin)
+    }
+    @IBAction func addWaterButtonAction(_ sender: UIButton) {
+        coffeeMachine.addIngredient(ingredient: .water)
+        
+        upDateProgress(currentProgress: progressWater)
+        upDateProgress(currentProgress: progressMilk)
+        upDateProgress(currentProgress: progressCoffee)
+        upDateProgress(currentProgress: progressBin)
+    }
+    @IBAction func addMilkButtonAction(_ sender: UIButton) {
+        coffeeMachine.addIngredient(ingredient: .milk)
+        
+        upDateProgress(currentProgress: progressWater)
+        upDateProgress(currentProgress: progressMilk)
+        upDateProgress(currentProgress: progressCoffee)
+        upDateProgress(currentProgress: progressBin)
+    }
+    @IBAction func addCoffeeButtonAction(_ sender: UIButton) {
+        coffeeMachine.addIngredient(ingredient: .coffee)
+        
+        upDateProgress(currentProgress: progressWater)
+        upDateProgress(currentProgress: progressMilk)
+        upDateProgress(currentProgress: progressCoffee)
+        upDateProgress(currentProgress: progressBin)
+    }
+    @IBAction func emptyBinButtonAction(_ sender: UIButton) {
+        coffeeMachine.addIngredient(ingredient: .trash)
+        
+        upDateProgress(currentProgress: progressWater)
+        upDateProgress(currentProgress: progressMilk)
+        upDateProgress(currentProgress: progressCoffee)
+        upDateProgress(currentProgress: progressBin)
+    }
+    
 }
 
